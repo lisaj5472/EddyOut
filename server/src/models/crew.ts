26613ba11484c633db849bbf5a2e4353f1d809cc
Justin Vittitoe <justinvittitoe@Justins-MacBook-Air-2.local>
2025-04-03 21:28:05 -0600

    created the crew model

---
+++ server/src/models/crew.ts
@@ -0,0 +1,56 @@
+import { DataTypes, type Sequelize, Model, type Optional } from "sequelize";
+import { Trip } from "./trip";
+
+//Create the items that are unique to crew
+interface CrewAttributes {
+    id: number;
+    username: string;
+    email: string;
+    tripName: string;
+    tripid:number;
+}
+
+
+
+//extends the trip information associated with the crew member
+export class Crew extends Model<CrewAttributes, Trip> {
+    public id!: string;
+    public username!: string;
+    public email!: string;
+    public tripname!: string;
+    public tripid!: number;
+
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;    
+}
+
+export function CrewFactory(sequelize: Sequelize): typeof Crew {
+    Crew.init({
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey:true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        tripName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        tripid: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+{
+    tableName: 'crew',
+    sequelize,
+});
+return Crew
+}
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,9 +2,14 @@ import sequelize from '../config/connection.js'
 import { TripFactory } from './trip.js'
 import { MealFactory } from './meals.js'
 import { UserFactory } from './user.js'
+import { CrewFactory } from './crew.js'
 
 const Trip = TripFactory(sequelize)
+const Crew = CrewFactory(sequelize)
 const Meal = MealFactory(sequelize)
 const User = UserFactory(sequelize)
 
-export { Trip, Meal, User }
+
+
+
+export { Trip, Crew, Meal, User }
