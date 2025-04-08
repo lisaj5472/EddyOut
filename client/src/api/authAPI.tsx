bc47f8fc22c99a06d73a0014e45a30f9a1be00bf
LisaJ5472 <lisaj5472@gmail.com>
2025-04-07 20:42:31 -0600

    completed front-end api for login, dashboard, and newtrip pages

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,26 +1,23 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 export const login = async (body: UserLogin) => {
-    try {
-        const response = await fetch(
-            'auth/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(body)
-            }
-        )
-        const data = await response.json();
+  try {
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
 
-        if(!response.ok) {
-            throw new Error('Invalid user login, please try again');
-        }
-
-        return data
-    } catch (err) {
-        console.log('Error with login')
-        return Promise.reject('Could not login user')
+    if (!response.ok) {
+      throw new Error("Invalid user login, please try again");
     }
-}
 
+    return data;
+  } catch (err) {
+    console.log("Error with login", err);
+    return Promise.reject("Could not login user");
+  }
+};
--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,13 +1,8 @@
-// Importing specific types and functions from the 'jwt-decode' library.
-// JwtPayload: A type definition representing the structure of a JSON Web Token payload.
-// jwtDecode: A function used to decode a JSON Web Token (JWT) and extract its payload.
-import { type JwtPayload, jwtDecode } from "jwt-decode";
+import type { JwtPayload } from "jwt-decode";
 import type { UserData } from "../interfaces/UserData";
-
+import { jwtDecode } from "jwt-decode";
 class AuthService {
   getProfile() {
-    // Decode the JSON Web Token (JWT) using the jwtDecode function, specifying the expected payload type as UserData.
-    // The getToken() method is called to retrieve the JWT, which is then passed to jwtDecode to extract and return its payload.
     return jwtDecode<UserData>(this.getToken());
   }
 
@@ -18,17 +13,13 @@ class AuthService {
 
   isTokenExpired(token: string) {
     try {
-      // Attempt to decode the provided token using jwtDecode, expecting a JwtPayload type.
       const decoded = jwtDecode<JwtPayload>(token);
 
-      // Check if the decoded token has an 'exp' (expiration) property and if it is less than the current time in seconds.
       if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
-        // If the token is expired, return true indicating that it is expired.
         return true;
       }
-    } catch (err) {
-      // If decoding fails (e.g., due to an invalid token format), catch the error and return false.
-      return false;
+    } catch {
+      return true;
     }
   }
 
@@ -39,12 +30,10 @@ class AuthService {
 
   login(idToken: string) {
     localStorage.setItem("id_token", idToken);
-    window.location.assign("/");
   }
 
   logout() {
     localStorage.removeItem("id_token");
-    window.location.assign("/");
   }
 }
 
