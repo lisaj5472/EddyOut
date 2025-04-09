66354cff28135f0c918b4f531dd4f195a2c7d14e
ellimckinley <190907228+ellimckinley@users.noreply.github.com>
2025-04-08 22:21:38 -0600

    removed id from all seed files

--- server/src/seeds/meal-seeds.ts
+++ server/src/seeds/meal-seeds.ts
@@ -4,42 +4,36 @@ import { Meals } from "../models/meals";
 export const seedMeals = async () => {
     await Meals.bulkCreate([
         {
-            id: 1,
             mealName: "Breakfast Burritos",
             mealType: "Breakfast",
             crewMember: "Justin",
             tripId: 1
         },
         {
-            id: 2,
             mealType: "Lunch",
             mealName: "Curry Chicken Wrap",
             crewMember: "Lisa",
             tripId: 1
         },
         {
-            id: 3,
             mealType: "Dinner",
             mealName: "Surf n Turf",
             crewMember: "Justin",
             tripId: 1
         },
         {
-            id: 4,
             mealType: "Breakfast",
             mealName: "Lox and Bagels",
             crewMember: "Elli",
             tripId: 1
         },
         {
-            id: 5,
             mealType: "Lunch",
             mealName: "Sesame Noodles",
             crewMember: "Elli",
             tripId: 1
         },
         {
-            id: 6,
             mealType: "Dinner",
             mealName: "Fancy Mac and Cheese",
             crewMember: "Lisa",
